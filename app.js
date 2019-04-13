222cbcc229e3e8127bb57c28f258165636657586
Richard Green <richardgreen@pursuit.org>
2019-04-12 22:55:34 -0400

    added routes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const {create,read,update, remove} = require('./routes/sellers');
 const {createListing, readListing, updateListing, removeListing} =require('./routes/listings')
 const {createBuyer, readBuyer, updateBuyer, removeBuyer} = require('./routes/buyer')
 const app = express();
-const firebase = require('firebase');
+const firebase = require('./firebase');
 
 // middleware
 app.use(bodyParser.urlencoded({extended:false}))
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 const checkFireBaseToken = (req, res, next) => {
     const {token} = req.body;
     console.log('token:', token)
-        admin.auth().verifyIdToken(token)
+    firebase.auth().verifyIdToken(token)
         .then(function(decodedToken) {
         var uid = decodedToken.uid;
             next();
@@ -21,6 +21,19 @@ const checkFireBaseToken = (req, res, next) => {
             res.json('ERROR!!!!')
         });
 }
+
+// const validUser = (req, res, next) => { 
+//     // GET THE USERS UID
+//     const uidInPostgres = '';
+    
+//     const uidInDecodedToken = req.decodedToken.uid;
+    
+//     if (uidInPostgres === uidInDecodedToken) {
+//     next();
+//     } else {
+//     res.json({ error: "You're not the valid user" });
+//         }
+//     }
 
 // routes
 
@@ -30,7 +43,7 @@ app.get('/seller/search', read);
 app.put('/seller/update', checkFireBaseToken, update);
 app.delete('/seller/delete', checkFireBaseToken, remove);
 // listings
-app.post('/listings/create',checkFireBaseToken,createListing); 
+app.post('/listings/create',createListing); //checkFireBaseToken
 app.get('/listings/search', readListing);
 app.put('/listings/update', checkFireBaseToken, updateListing);
 app.delete('/listings/delete',checkFireBaseToken ,removeListing);
@@ -39,5 +52,6 @@ app.post('/buyer/create', createBuyer);
 app.get('/buyer/search', checkFireBaseToken, readBuyer);
 app.put('/buyer/update', checkFireBaseToken, updateBuyer);
 app.delete('/buyer/delete', checkFireBaseToken, removeBuyer);
+
 
 module.exports = { app, }
